60b20353117792af624342a31d008e345c0fc91c
nileshp-07 <138235105+nileshp-07@users.noreply.github.com>
2024-02-04 18:28:02 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -32,6 +32,16 @@ app.use(
     })
 )
 
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://study-notion-edu-tech-platform-6wsz.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(
     fileUpload({
         useTempFiles : true,
